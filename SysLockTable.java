4b252143a852cb52f934c4039e988c44fc52c977
Bruna <bruna_gcarneiro@hotmail.com>
2024-09-08 19:06:33 -0300

    tabela syslock

--- SysLockTable.java
+++ SysLockTable.java
@@ -1,3 +1,8 @@
 public class SysLockTable {
-    
+  String[][] SysLock;
+
+  public SysLockTable() {
+  SysLock = new String[1][5];
+  SysLock[0] = new String[]{"tId", "objId", "objType", "blockType", "status"};
+  }
 }
